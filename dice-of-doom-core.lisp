f70cf2ec031ddfbfcc67243c60b926c935a056ae
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2020-02-11 17:31:43 +0900

    Implemenet dice-of-doom-v2 using lazy evaluation

---
+++ dice-of-doom-core.lisp
@@ -0,0 +1,83 @@
+(defparameter *num-players* 2)
+(defparameter *max-dice* 3)
+(defparameter *board-size* 3)
+(defparameter *board-hexnum* (* *board-size* *board-size*))
+
+(defun board-array (lst)
+  (make-array *board-hexnum* :initial-contents lst))
+
+(defun gen-board ()
+  (board-array (loop for n below *board-hexnum*
+                     collect (list (random *num-players*)
+                                   (1+ (random *max-dice*))))))
+
+(defun player-letter (n)
+  (code-char (+ 97 n)))
+
+(defun draw-board (board)
+  (loop for y below *board-size*
+        do (progn (fresh-line)
+                  (loop repeat (- *board-size* y)
+                        do (princ " "))
+                  (loop for x below *board-size*
+                        for hex = (aref board (+ x (* *board-size* y)))
+                        do (format t "~a ~a " (player-letter (first hex))
+                                   (second hex))))))
+
+(defun game-tree (board player spare-dice first-move)
+  (list player
+        board
+        (add-passing-move board
+                          player
+                          spare-dice
+                          first-move
+                          (attacking-moves board player spare-dice))))
+
+(defun neighbors (pos)
+  (let ((up (- pos *board-size*))
+        (down (+ pos *board-size*)))
+    (loop for p in (append (list up down)
+                           (unless (zerop (mod pos *board-size*))
+                             (list (1- up) (1- pos)))
+                           (unless (zerop (mod (1+ pos) *board-size*))
+                             (list (1+ pos) (1+ down))))
+          when (and (>= p 0) (< p *board-hexnum*))
+          collect p)))
+
+(defun print-info (tree)
+  (fresh-line)
+  (format t "current player = ~a" (player-letter (car tree)))
+  (draw-board (cadr tree)))
+
+(defun winners (board)
+  (let* ((tally (loop for hex across board
+                      collect (car hex)))
+         (totals (mapcar (lambda (player)
+                           (cons player (count player tally)))
+                         (remove-duplicates tally)))
+         (best (apply #'max (mapcar #'cdr totals))))
+    (mapcar #'car
+            (remove-if (lambda (x)
+                         (not (eq (cdr x) best)))
+                       totals))))
+
+(defun announce-winner (board)
+  (fresh-line)
+  (let ((w (winners board)))
+    (if (> (length w) 1)
+      (format t "The game is a tie between ~a" (mapcar #'player-letter w))
+      (format t "The winner is ~a" (player-letter (car w))))))
+
+(let ((old-neighbors (symbol-function 'neighbors))
+      (previous (make-hash-table)))
+  (defun neighbors (pos)
+    (or (gethash pos previous)
+        (setf (gethash pos previous) (funcall old-neighbors pos)))))
+
+(let ((old-game-tree (symbol-function 'game-tree))
+      (previous (make-hash-table :test #'equalp)))
+  (defun game-tree (&rest rest)
+    (or (gethash rest previous)
+        (setf (gethash rest previous) (apply old-game-tree rest)))))
+
+
--- dice-of-doom.lisp
+++ dice-of-doom.lisp
@@ -1,37 +1,4 @@
-(defparameter *num-players* 2)
-(defparameter *max-dice* 3)
-(defparameter *board-size* 3)
-(defparameter *board-hexnum* (* *board-size* *board-size*))
-
-(defun board-array (lst)
-  (make-array *board-hexnum* :initial-contents lst))
-
-(defun gen-board ()
-  (board-array (loop for n below *board-hexnum*
-                     collect (list (random *num-players*)
-                                   (1+ (random *max-dice*))))))
-
-(defun player-letter (n)
-  (code-char (+ 97 n)))
-
-(defun draw-board (board)
-  (loop for y below *board-size*
-        do (progn (fresh-line)
-                  (loop repeat (- *board-size* y)
-                        do (princ " "))
-                  (loop for x below *board-size*
-                        for hex = (aref board (+ x (* *board-size* y)))
-                        do (format t "~a ~a " (player-letter (first hex))
-                                   (second hex))))))
-
-(defun game-tree (board player spare-dice first-move)
-  (list player
-        board
-        (add-passing-move board
-                          player
-                          spare-dice
-                          first-move
-                          (attacking-moves board player spare-dice))))
+(load 'dice-of-doom-core)
 
 (defun add-passing-move (board player spare-dice first-move moves)
   (if first-move
@@ -62,17 +29,6 @@
                         (neighbors src))))
             (loop for n below *board-hexnum*
                   collect n))))
-
-(defun neighbors (pos)
-  (let ((up (- pos *board-size*))
-        (down (+ pos *board-size*)))
-    (loop for p in (append (list up down)
-                           (unless (zerop (mod pos *board-size*))
-                             (list (1- up) (1- pos)))
-                           (unless (zerop (mod (1+ pos) *board-size*))
-                             (list (1+ pos) (1+ down))))
-          when (and (>= p 0) (< p *board-hexnum*))
-          collect p)))
 
 (defun board-attack (board player src dst dice)
   (board-array (loop for pos from 0
@@ -113,11 +69,6 @@
     (play-vs-human (handle-human tree))
     (announce-winner (cadr tree))))
 
-(defun print-info (tree)
-  (fresh-line)
-  (format t "current player = ~a" (player-letter (car tree)))
-  (draw-board (cadr tree)))
-
 (defun handle-human (tree)
   (fresh-line)
   (princ "choose your move:")
@@ -132,25 +83,6 @@
                  (princ "end turn"))))
     (fresh-line)
     (cadr (nth (1- (read)) moves))))
-
-(defun winners (board)
-  (let* ((tally (loop for hex across board
-                      collect (car hex)))
-         (totals (mapcar (lambda (player)
-                           (cons player (count player tally)))
-                         (remove-duplicates tally)))
-         (best (apply #'max (mapcar #'cdr totals))))
-    (mapcar #'car
-            (remove-if (lambda (x)
-                         (not (eq (cdr x) best)))
-                       totals))))
-
-(defun announce-winner (board)
-  (fresh-line)
-  (let ((w (winners board)))
-    (if (> (length w) 1)
-      (format t "The game is a tie between ~a" (mapcar #'player-letter w))
-      (format t "The winner is ~a" (player-letter (car w))))))
 
 (defun rate-position (tree player)
   (let ((moves (caddr tree)))
@@ -178,18 +110,6 @@
   (cond ((null (caddr tree)) (announce-winner (cadr tree)))
         ((zerop (car tree)) (play-vs-computer (handle-human tree)))
         (t (play-vs-computer (handle-computer tree)))))
-
-(let ((old-neighbors (symbol-function 'neighbors))
-      (previous (make-hash-table)))
-  (defun neighbors (pos)
-    (or (gethash pos previous)
-        (setf (gethash pos previous) (funcall old-neighbors pos)))))
-
-(let ((old-game-tree (symbol-function 'game-tree))
-      (previous (make-hash-table :test #'equalp)))
-  (defun game-tree (&rest rest)
-    (or (gethash rest previous)
-        (setf (gethash rest previous) (apply old-game-tree rest)))))
 
 (let ((old-rate-position (symbol-function 'rate-position))
       (previous (make-hash-table)))
